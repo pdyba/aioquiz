6ee07422d34133f1f99439bbbef3c77487f71515
Piotr Dyba <piotr@dyba.com.pl>
2017-09-03 22:42:49 +0200

    bug fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1357,7 +1357,7 @@ function run($rootScope, $location, $cookies, $http) {
 
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
         // redirect to login page if not logged in and trying to access a restricted page
-        var restrictedPage = $.inArray($location.path(), ['/login', '/register', '/about', '/', '/rules', '/regconfirmed']) === -1;
+        var restrictedPage = $.inArray($location.path(), ['/login', '/register', '/about', '/', '/rules',  '/program', '/regconfirmed']) === -1;
         var loggedIn = $rootScope.globals.currentUser;
         if (restrictedPage && !loggedIn) {
             $location.path('/');
