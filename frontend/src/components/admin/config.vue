b4416b3a74e7fad1225f2661d190841ceff08165
Piotr Dyba <piotr@dyba.com.pl>
2018-04-09 23:28:21 +0200

    adds config some small additions for admins users

--- frontend/src/components/admin/config.vue
+++ frontend/src/components/admin/config.vue
@@ -1,50 +1,28 @@
 <template>
-    <div class="container" xmlns="http://www.w3.org/1999/html">
+    <div class="container">
 
         <div class="col-md-6 col-md-offset-3">
             <h3>Config:</h3>
-            <form name="form"  role="form">
+            <b-form>
+                <b-form-group label="Room rows" id="email_type">
+                    <b-form-input type="number" v-model="config.room_raws"></b-form-input>
+                </b-form-group>
 
+                <b-form-group label="Room Columns" id="email_type">
+                    <b-form-input type="number" v-model="config.room_columns"></b-form-input>
+                </b-form-group>
 
-                <div class="form-group"
-                     >
-                    <label for="room_raws">Raws</label>
-                    <p>You need to input 1 and 9999</p>
-
-                    <input name="room_raws" id="room_raws" type="number" v-model="config.room_raws"
-                           v-pattern="/^[0-9]{1,4}$/"/>
-                </div>
-
-                <div class="form-group"
-                     >
-                    <label for="room_columns">Columns</label>
-                    <span 
-                          class="help-inline alert-danger">
-                    <p>You need to input 1 and 9999</p>
-                </span>
-                    <input name="room_columns" id="room_columns" type="number" v-model="config.room_columns"
-                           v-pattern="/^[0-9]{1,4}$/"/>
-                </div>
-
-
-                <div class="checkbox">
-                    <label for="reg_active">
-                        <input type="checkbox" name="reg_active"
-                               id="reg_active"
-                               v-model="config.reg_active"/>
+                <b-form-group label="Registration status:" id="email_type">
+                    <b-form-checkbox v-model="config.reg_active">
                         <p v-if="config.reg_active">Rejetracja jest teraz aktywna !</p>
                         <p v-if="!config.reg_active">Rejetracja nie jest teraz aktywna !</p>
-                    </label>
-                </div>
+                    </b-form-checkbox>
+                </b-form-group>
 
                 <div class="form-actions">
-                    <button type="submit"
-                            class="btn btn-primary">Update
-                    </button>
-                    <img v-if="dataLoading"
-                         src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
+                    <b-btn variant="primary" @click.prevent="save_config()">Update</b-btn>
                 </div>
-            </form>
+            </b-form>
         </div>
     </div>
 
@@ -52,16 +30,32 @@
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
         name: "admin_config",
-        data: () => {
+        data() {
             return {
                 dataLoading: false,
-                config: {
-                    room_rows: 1,
-                    room_columns: 1,
-                    reg_active: true
+                config: {}
+            }
+        },
+        created() {
+            let self = this;
+            axios.get('/admin/config').then((resp) => {
+                    self.config = resp.data;
                 }
+            );
+        },
+        methods: {
+            save_config() {
+                let self = this;
+                axios.post('/admin/config', self.config).then((resp) => {
+                    self.$swal({
+                        title: resp.data.msg,
+                        type: 'success'
+                    });
+                });
             }
         }
     }
--- frontend/src/components/admin/users.vue
+++ frontend/src/components/admin/users.vue
@@ -1,10 +1,10 @@
 <template>
     <container>
+
         <h1 class="page-header">Admin: Users</h1>
         <h3>Stats</h3>
         <b-table hover small :items="users_stats" :fields="fields_stats">
         </b-table>
-
 
         <h3>All registered users:</h3>
         <b-table hover small :items="allUsers" :fields="fields_users">
@@ -12,62 +12,6 @@
                 <usersActions :user="cell.item"></usersActions>
             </template>
         </b-table>
-        <!--<div class="table-responsive">-->
-        <!--<table class="table">-->
-        <!--<thead>-->
-        <!--<tr>-->
-        <!--<th>ID</th>-->
-        <!--<th>E-mail</th>-->
-        <!--<th>Name</th>-->
-        <!--<th>Surname</th>-->
-        <!--<th>Active</th>-->
-        <!--<th>Delete</th>-->
-        <!--<th>Make Organiser</th>-->
-        <!--<th>Make Mentor</th>-->
-        <!--<th>New Password</th>-->
-        <!--</tr>-->
-        <!--</thead>-->
-
-        <!--<tbody>-->
-        <!--<tr v-for="user in allUsers"-->
-        <!--v-class="{'danger': user.admin, 'success': user.mentor, 'warning': user.organiser, 'info': !user.mentor}">-->
-        <!--<td>{{ user.id }}</td>-->
-        <!--<td>{{ user.email }}</td>-->
-        <!--<td>{{ user.name }}</td>-->
-        <!--<td>{{ user.surname }}</td>-->
-        <!--<td v-if="!user.active"><a v-click="makeActive(user)"-->
-        <!--class="btn btn-xs btn-success">Make Active</a></td>-->
-        <!--td>-->
-        <!--<td v-if="user.active && !user.admin"><a v-click="makeInactive(user)"-->
-        <!--class="btn btn-xs btn-danger">Deactivate</a></td>-->
-        <!--td-->
-        <!--<td v-if="user.admin">Yeah</td>-->
-        <!--<td><a v-hide="user.admin" v-click="deleteUser(user.id)"-->
-        <!--class="btn btn-xs btn-danger">Delete</a>-->
-        <!--<x v-show="user.admin">Nope</x>-->
-        <!--</td>-->
-        <!--<td><a v-if="!user.admin" v-show="!user.organiser" v-click="makeOrganiser(user)"-->
-        <!--class="btn btn-xs btn-primary">Make Organiser</a>-->
-        <!--<a v-hide="user.admin || !user.organiser" v-show="user.organiser"-->
-        <!--v-click="removeOrganiser(user)"-->
-        <!--class="btn btn-xs btn-danger">Remove Organiser</a>-->
-        <!--<x v-show="user.admin">Nope</x>-->
-
-        <!--<td><a v-hide="user.mentor || user.admin" v-click="makeMentor(user)"-->
-        <!--class="btn btn-xs btn-info">Make Mentor</a>-->
-        <!--<a v-show="user.mentor && !user.admin" v-click="removeMentor(user)"-->
-        <!--class="btn btn-xs btn-info">Remove Mentor</a>-->
-        <!--<x v-show="user.admin">Nope</x>-->
-        <!--</td>-->
-        <!--<td>-->
-        <!--<a v-show="!user.admin" v-click="newPassword(user)"-->
-        <!--class="btn btn-xs btn-info">NewPass</a>-->
-        <!--<x v-show="user.admin">Nope</x>-->
-        <!--</td>-->
-        <!--</tr>-->
-        <!--</tbody>-->
-        <!--</table>-->
-        <!--</div>-->
 
     </container>
 </template>
