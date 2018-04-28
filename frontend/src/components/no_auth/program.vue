0a4e5b5d9503e21907934e5bde9d39a4c24f8864
Piotr Dyba <piotr@dyba.com.pl>
2018-04-28 16:41:51 +0200

    fixes some bugs

--- frontend/src/components/no_auth/program.vue
+++ frontend/src/components/no_auth/program.vue
@@ -163,92 +163,92 @@
                         'Hermetyzacja dzieci – rodzaje metod'
                     ],
                 tab_2: [
-                    {'date': '2017.10.17', 'subject': 'Organizacja i powtórka ', 'author': ' Piotr Dyba'},
-                    {'date': '2017.10.24', 'subject': 'Praca z ptdkami, format JSON ', 'author': ' Piotr Dyba'},
+                    {'date': '2017.10.17', 'subject': 'Organizacja i powtórka ', 'author': 'Piotr Dyba'},
+                    {'date': '2017.10.24', 'subject': 'Praca z ptdkami, format JSON ', 'author': 'Piotr Dyba'},
                     {
                         'date': '2017.11.07',
                         'subject': 'Bibtdoteki zewnętrzne, praca z API RESTowym ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     },
-                    {'date': '2017.11.14', 'subject': 'Git - system kontrotd wersji ', 'author': ' Magdalena Nowak'},
+                    {'date': '2017.11.14', 'subject': 'Git - system kontrotd wersji ', 'author': 'Magdalena Nowak'},
                     {
                         'date': '2017.11.21',
-                        'subject': 'Wszystko jest obiektem wstęp do programowania obiektowego ', 'author': ' Piotr Dyba'
+                        'subject': 'Wszystko jest obiektem wstęp do programowania obiektowego ', 'author': 'Piotr Dyba'
                     },
-                    {'date': '2017.11.28', 'subject': 'Wszytko jest obiektem - cd. ', 'author': ' Piotr Dyba'},
+                    {'date': '2017.11.28', 'subject': 'Wszytko jest obiektem - cd. ', 'author': 'Piotr Dyba'},
                     {
                         'date': '2017.12.05',
-                        'subject': 'Aptdkacja webowa w Pythonie, wstęp do Flaska ', 'author': ' Grzegorz Prokopczyk'
+                        'subject': 'Aptdkacja webowa w Pythonie, wstęp do Flaska ', 'author': 'Grzegorz Prokopczyk'
                     },
                     {
                         'date': '2017.12.12',
-                        'subject': 'HTML oraz tworzenie wzorników stron i formularzy ', 'author': ' Grzegorz Prokopczyk'
+                        'subject': 'HTML oraz tworzenie wzorników stron i formularzy ', 'author': 'Grzegorz Prokopczyk'
                     },
                     {
                         'date': '2018.01.09',
                         'subject': 'Powtórka: pierwsza pełna aptdkacja webowa, Jinja2 ',
-                        'author': ' Grzegorz Prokopczyk'
+                        'author': 'Grzegorz Prokopczyk'
                     },
                     {
                         'date': '2018.01.16',
                         'subject': 'Powtórka całości ',
-                        'author': ' Krzysztof Marciniak, Piotr Dyba'
+                        'author': 'Krzysztof Marciniak, Piotr Dyba'
                     },
                     {
                         'date': '2018.01.23',
                         'subject': 'PEP8, Pytdnt i metodologie programistyczne ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     },
                     {
                         'date': '2018.01.30',
                         'subject': 'SQLAlchemy oraz wybór projektów zespołowych ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     },
                     {'date': '2018.02.06', 'subject': 'BRAK ZAJĘĆ'},
-                    {'date': '2018.02.13', 'subject': 'Bazy danych SQL ', 'author': ' Grzegorz Prokopczyk'},
+                    {'date': '2018.02.13', 'subject': 'Bazy danych SQL ', 'author': 'Grzegorz Prokopczyk'},
                     {'date': '2018.02.20', 'subject': 'BRAK ZAJĘĆ'},
                     {
                         'date': '2018.02.27',
                         'subject': 'Tworzenie API RESTowego oraz Przechowywanie i tworzenie ptdków konfiguracyjnych ',
-                        'author': ' Grzegorz Prokopczyk oraz Piotr Dyba'
+                        'author': 'Grzegorz Prokopczyk oraz Piotr Dyba'
                     }
                 ],
                 tab_3: [
                     {
                         'date': '2018.03.06',
                         'subject': 'Scrum, Agile i omówienie projektów zespołowych ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     },
                     {'date': '2018.03.13', 'subject': 'Systemy kolejkowe i bazy danych NOSQL'},
                     {
                         'date': '2018.03.20',
                         'subject': 'Testy Jednostkowe i TDD ',
-                        'author': ' Aleksandra Kornecka, Grzegorz Prokopczyk'
+                        'author': 'Aleksandra Kornecka, Grzegorz Prokopczyk'
                     },
-                    {'date': '2018.03.27', 'subject': 'Zaawansowane konstrukty w Pythonie ', 'author': ' Piotr Dyba'},
+                    {'date': '2018.03.27', 'subject': 'Zaawansowane konstrukty w Pythonie ', 'author': 'Piotr Dyba'},
                     {'date': '2018.04.03', 'subject': 'BRAK ZAJĘĆ'},
-                    {'date': '2018.04.10', 'subject': 'Wyrażenie regularne ', 'author': ' Zuzanna Kunik'},
-                    {'date': '2018.04.17', 'subject': 'Linux, Bash i SSH wprowadzenie ', 'author': ' Filip Rachwalak'},
+                    {'date': '2018.04.10', 'subject': 'Wyrażenie regularne ', 'author': 'Zuzanna Kunik'},
+                    {'date': '2018.04.17', 'subject': 'Linux, Bash i SSH wprowadzenie ', 'author': 'Filip Rachwalak'},
                     {
                         'date': '2018.04.24',
                         'subject': 'Deployment aptdkacji webowej ',
-                        'author': ' Grzegorz Prokopczyk'
+                        'author': 'Grzegorz Prokopczyk'
                     },
                     {'date': '2018.05.01', 'subject': 'BRAK ZAJĘĆ'},
-                    {'date': '2018.05.08', 'subject': 'VirtualEnv', 'author': ' Piotr Dyba'},
-                    {'date': '2018.05.15', 'subject': 'Zaawansowane funkcje w Pythonie', 'author': ' Piotr Dyba'},
-                    {'date': '2018.05.22', 'subject': 'Webapp security 101', 'author': ' Krzysztof Marciniak'},
+                    {'date': '2018.05.08', 'subject': 'VirtualEnv', 'author': 'Piotr Dyba'},
+                    {'date': '2018.05.15', 'subject': 'Zaawansowane funkcje w Pythonie', 'author': 'Piotr Dyba'},
+                    {'date': '2018.05.22', 'subject': 'Webapp security 101', 'author': 'Krzysztof Marciniak'},
                     {'date': '2018.05.29', 'subject': 'BRAK ZAJĘĆ'},
-                    {'date': '2018.06.05', 'subject': 'Docker', 'author': ' Marcin Tamioła'},
+                    {'date': '2018.06.05', 'subject': 'Docker', 'author': 'Marcin Tamioła'},
                     {
                         'date': '2018.06.12',
                         'subject': 'Test sprawdzający, przygotowanie pod staż ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     },
                     {
                         'date': '2018.06.19',
                         'subject': 'Prezentacja i omówienie projektów, wyniki testów, rozdanie dyplomów ',
-                        'author': ' Piotr Dyba'
+                        'author': 'Piotr Dyba'
                     }
                 ]
             }
