8bfbd26fc54284f3bd84793a616cf6b7f45899d1
Piotr Dyba <piotr@dyba.com.pl>
2018-05-16 23:52:55 +0200

    removes live quiz from grading names properly mentor grading views

--- views/mentor/competence_tests.py
+++ views/mentor/competence_tests.py
@@ -2,21 +2,15 @@
 # encoding: utf-8
 from views.mentor.common import CommonMentorTestBase
 
-from models import LiveQuiz
 from models import Quiz
 from models import Exam
 
 
-class QuizOrgBase(CommonMentorTestBase):
+class QuizMentorBase(CommonMentorTestBase):
     _cls = Quiz
     _urls = ['/api/mentor/quiz', '/api/mentor/quiz/<tid:int>']
 
 
-class LiveQuizOrgBase(CommonMentorTestBase):
-    _cls = LiveQuiz
-    _urls = ['/api/mentor/live_quiz', '/api/mentor/live_quiz/<tid:int>']
-
-
-class ExamOrgBase(CommonMentorTestBase):
+class ExamMentorBase(CommonMentorTestBase):
     _cls = Exam
     _urls = ['/api/mentor/exam/', '/api/mentor/exam/<tid:int>']
